4466532f17362ecd1c0d02284bcce33e09d4d78e
AlKurpiakov <s23380350@unn.ru>
2024-10-08 10:10:16 +0300

    123

--- BeginTask-0/ArrayHandler.h
+++ BeginTask-0/ArrayHandler.h
@@ -1,35 +1,3 @@
-// #include <cstddef>
-// #include <cstring>
-// #include <cstdint>
-// #include <limits>
-
-// template<typename T>
-// class ArrayHandler {
-// private:
-//     T* _array;
-//     size_t _size;
-//     size_t _count;
-//     T _min;
-//     T _max;
-
-// public:
-//     ArrayHandler(size_t initial_size = 1000000)
-//         : _size(initial_size), _count(0), _min(std::numeric_limits<T>::max()), _max(std::numeric_limits<T>::min()) {
-//         _array = new T[_size];
-//     }
-
-//     void AppendElem(T elem);
-//     T GetMax() const ;
-
-//     T GetMin() const {
-//         return _min;
-//     }
-
-//     bool IsContains(T __restrict* elem) const;
-
-//     ~ArrayHandler();
-// };
-
 #include <cstddef>
 #include <cstring>
 #include <cstdint>
