815b843d0a66e6194aa2ebb6e86c6dd6b58c3b0d
AlKurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2024-10-15 14:52:11 +0300

    Update main.cpp

--- BeginTask-0/main.cpp
+++ BeginTask-0/main.cpp
@@ -6,6 +6,7 @@
 int main(int argc, char **argv) {
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
+    std::cout.tie(0);
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
