c4efcb4240f242969eea425d4a6e9122436b0beb
AlKurpiakov <s23380350@unn.ru>
2024-11-07 17:55:28 +0300

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   BitField.cpp 	modified:   BitField.h 	modified:   Set.cpp 	modified:   Set.h 	modified:   tests.cpp

--- BitField-2/Set.cpp
+++ BitField-2/Set.cpp
@@ -1,56 +1,112 @@
 #include "Set.h"
 
-
-Set::Set(size_t mp) : _bitField(10) {
-
-}
-Set::Set(const Set &s) : _bitField(10){
-
-} 
-Set::Set(const BitField &bf) : _bitField(10){
-
+Set::operator BitField() {
+    return BitField(this->_bitfield);
 }
 
-size_t Set::GetMaxPower() const{
-    return 0;
-}    
-void Set::InsElem(const uint64_t Elem){
+Set::Set(const BitField& tmp) : _maxPower(tmp.GetLength()), _bitfield(tmp) {}
 
-}
-void Set::DelElem(const uint64_t Elem){
+Set::Set(const Set& tmp):_maxPower(tmp.GetMaxPow()), _bitfield(tmp._bitfield) {}
 
+Set::Set(const size_t maxPower) :_bitfield(maxPower) { _maxPower = maxPower; }
+
+void Set::InsElem(uint64_t elem) {
+    if (elem < _maxPower)
+        _bitfield.SetBit(elem);
+    else {
+        throw "Elem out of range";
+    }
 }
-bool Set::IsMember(const uint64_t Elem) const{
-    return 0;
+
+void Set::DelElem(uint64_t elem) {
+    _bitfield.ClrBit(elem);
+}
+
+size_t Set::GetMaxPow() const {
+    return _maxPower;
+}
+
+bool Set::IsMember(uint64_t elem) {
+    if (elem < _maxPower && elem >= 0){
+        if (_bitfield.GetBit(elem) == 1) return true;
+        }
+        return false;
 }
 
 
-bool Set::operator== (const Set &s) const{
-    return 0;
+bool Set::operator==(const Set& tmp) const {
+    if (_bitfield == tmp._bitfield)
+        return true;
+    return false;
 }
-bool Set::operator!= (const Set &s) const{
-    return 0;
+
+bool Set::operator!=(const Set& tmp) const{
+    if (_bitfield == tmp._bitfield)
+        return false;
+    return true;
 }
-Set& Set::operator=(const Set &s){
+
+Set& Set::operator=(const Set& tmp) {
+    _maxPower = tmp._maxPower;
+    _bitfield = tmp._bitfield;
     return *this;
 }
-Set Set::operator+ (const uint64_t Elem){
-    return *this;
+
+Set Set::operator+(const Set& tmp) {
+    Set result(max(tmp._maxPower, _maxPower));
+    result._bitfield = _bitfield | tmp._bitfield;
+    return result;
 }
-                                  
-Set Set::operator- (const uint64_t Elem){
-    return *this;
+
+vector<uint64_t> Set::GetPrimary() const {
+    vector <uint64_t> res;
+    unordered_set<uint64_t> used;
+    res.push_back(1);
+    for (uint64_t i = 2; i < _maxPower + 1; i++){
+    
+        if (used.find(i) != used.end()) continue;
+    
+        else{
+            res.push_back(i);
+            for (uint64_t j = i + 1; j < _maxPower + 1; j++)
+                if (j % i == 0) used.insert(j);
+            
+       }
+    }
+    return res;
 }
-                                   
-Set Set::operator+ (const Set &s){
-    return *this;
+
+
+void Set::operator+(uint64_t elem) {
+    InsElem(elem);
 }
-Set Set::operator* (const Set &s){
-    return *this;
+
+void Set::operator-(uint64_t elem) {
+    DelElem(elem);
 }
-Set Set::operator~ (){
-    return *this;
+
+Set Set::operator*(const Set& tmp) {
+    Set result(max(tmp._maxPower, _maxPower));
+    result._bitfield = _bitfield & tmp._bitfield;
+    return result;
 }
-std::vector<uint64_t> Set::GetPrimary(){
-    return std::vector<uint64_t>();
+
+Set Set::operator~() {
+    Set result(_maxPower);
+    result._bitfield = ~_bitfield;
+    return result;
 }
+
+
+ostream& operator<<(ostream& os, const Set& tmp) {
+    os << tmp._bitfield;
+    return os << endl;
+};
+
+istream& operator>>(istream& is, Set& tmp) {
+    is >> tmp._maxPower;
+    is >> tmp._bitfield;
+    return is;
+}
+
+
