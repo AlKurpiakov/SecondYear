be6e83755c009a60cb5c6dad2c3219a49ea65895
AlKurpiakov <166383450+AlKurpiakov@users.noreply.github.com>
2024-11-23 21:25:47 +0300

    Delete build directory

--- build/_deps/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/s23380350@unn.global/hsss/SecondYear/build/_deps/googletest-src/googlemock
-# Build directory: /home/s23380350@unn.global/hsss/SecondYear/build/_deps/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
